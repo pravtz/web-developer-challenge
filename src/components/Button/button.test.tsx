f643c23304324b4c52b3baf478f9e263de3d3424
pravtz <ederson.ufrgs@gmail.com>
2023-02-21 17:48:37 -0300

    test component button

--- src/components/Button/button.test.tsx
+++ src/components/Button/button.test.tsx
@@ -9,4 +9,31 @@ describe('#ButtonMain', () => {
 
         expect(button).toBeInTheDocument()
     })
+    it("should render with variant link", () => {
+        render(<ButtonMain variant="link" >any</ButtonMain>)
+
+        /* @ts-ignore */
+        const buttonMain = ButtonMain('any').type.styledComponentId
+        const themeButton = document.getElementsByClassName(buttonMain)
+        const style = window.getComputedStyle(themeButton[0])
+
+        expect(style.backgroundColor).toBe("transparent")
+
+
+
+    })
+    it("should render with variant solid", () => {
+        render(<ButtonMain variant="solid" >any</ButtonMain>)
+
+        /* @ts-ignore */
+        const buttonMain = ButtonMain('any').type.styledComponentId
+        const themeButton = document.getElementsByClassName(buttonMain)
+        const style = window.getComputedStyle(themeButton[0])
+
+        expect(style.backgroundColor).toBe("rgb(113, 187, 0)")
+
+
+
+    })
 })
+
